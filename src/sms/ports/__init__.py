8563aca66670ba9719c4f1b7de74ea387d0460a4
TheByronHimes <TheByronHimes@gmail.com>
2024-08-28 07:13:39 +0000

    Add __init__ file doc strings

--- src/sms/ports/__init__.py
+++ src/sms/ports/__init__.py
@@ -12,3 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Package containing abstract definitions (ports) of main domain and non-domain classes."""
